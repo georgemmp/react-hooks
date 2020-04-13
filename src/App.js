719452316141a8f8c9c71a1feb484cba73bf629e
George <machadogeorge.p@gmail.com>
2020-04-13 20:03:40 -0300

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [tech, setTech] = useState(['React.js', 'React Native', 'Nodejs']);
+  const [tech, setTech] = useState([]);
   const [newTech, setNewTech] = useState('');
 
   function handleAdd() {
     setTech([...tech, newTech]);
     setNewTech('');
   }
+
+  // Component did mount
+  useEffect(() => {
+    const storageTech = localStorage.getItem('techs');
+
+    if (storageTech) {
+      setTech(JSON.parse(storageTech));
+    }
+  }, []);
+
+  // Component did update
+  useEffect(() => {
+    localStorage.setItem('techs', JSON.stringify(tech));
+  }, [tech]);
 
   return (
     <>
@@ -16,7 +30,7 @@ function App() {
           <li key={item}>{item}</li>
         ))}
       </ul>
-      <input value={newTech} onChange={e => setNewTech(e.target.value)} />
+      <input value={newTech} onChange={(e) => setNewTech(e.target.value)} />
       <button type="button" onClick={handleAdd}>
         Adicionar
       </button>
